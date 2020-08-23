e8b57d3525fe6d11cadcd81db575a8cf9871bf48
kanhaiya8608 <57759817+kanhaiya8608@users.noreply.github.com>
2020-08-23 16:01:41 +0530

    Create Finding Numbers in a Haystack.py

---
+++ Using Python to Access Web Data/Finding Numbers in a Haystack.py
@@ -0,0 +1,16 @@
+"""
+Handling The Data
+The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then 
+converting the extracted strings to integers and summing up the integers.
+"""
+import re
+fh = open('regex_sum_283285.txt')
+sum = 0
+
+for line in fh:
+    numbers = re.findall('[0-9]+', line)
+    
+    for number in numbers:
+        sum = sum + int(number)
+
+print(sum)
