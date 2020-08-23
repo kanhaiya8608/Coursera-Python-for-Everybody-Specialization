197f638f645c6799639e2a82a8d0620fa7f9fc4e
kanhaiya8608 <57759817+kanhaiya8608@users.noreply.github.com>
2020-08-23 19:08:55 +0530

    Update Extracting Data from XML.py

--- Using Python to Access Web Data/Extracting Data from XML.py
+++ Using Python to Access Web Data/Extracting Data from XML.py
@@ -1,8 +1,36 @@
 """
-Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+Extracting Data from XML
+
+In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
 
 We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
 
-Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
-Actual data: http://py4e-data.dr-chuck.net/comments_283287.html (Sum ends with 97)
+Sample data: http://py4e-data.dr-chuck.net/comments_42.xml (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_283289.xml (Sum ends with 25)
 """
+"""input url = http://py4e-data.dr-chuck.net/comments_283289.xml"""
+import urllib
+from urllib import request
+import xml.etree.ElementTree as ET
+
+url = input("Enter - ")
+uh = urllib.request.urlopen(url)
+data = uh.read()
+
+tree = ET.fromstring(data)
+results = tree.findall('comments/comment')
+count =0
+sum=0
+for item in results:
+    x = int(item.find('count').text)
+    count =count+1
+    sum = sum+x
+
+print("Count:", count)
+print("Sum : ",sum)
+"""
+Output:
+Count: 50
+Sum :  2725
+"""
+
